c1b3601d158e4613dd41d41a541bd3c76c528c4c
Jack <git@5079.ml>
2021-02-27 00:53:57 -0500

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,10 @@
 /** Given an IP, will return if the IP is from Roblox or not. */
-declare function is_roblox_ip (ip: string): boolean
-
-export {
-  is_roblox_ip
+declare const is_roblox_ip: {
+  /** Given an IP, will return if the IP is from Roblox or not. */
+  (ip: string): boolean
+  /** Given an IP, will return if the IP is from Roblox or not. */
+  is_roblox_ip (ip: string): boolean
 }
-export default is_roblox_ip
+
+/** Given an IP, will return if the IP is from Roblox or not. */
+export = is_roblox_ip
